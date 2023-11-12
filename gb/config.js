14b344244b5ba2e9e7a3fff8164b069310fb5017
gilad <mictavim@gmail.com>
2023-11-12 11:41:44 +0200

    fix env locak

--- gb/config.js
+++ gb/config.js
@@ -1,37 +1,38 @@
+const mode = process.env.NODE_ENV || "development";
 
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+import dotenv from "dotenv"; // Import dotenv using ES6 import
 
-
-const mode = process.env.MODE || 'development';
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
-
 const baseConfig = {
-	database: {
-		MY_SQL_HOST: process.env.MY_SQL_HOST,
-		MY_SQL_USER: process.env.MY_SQL_USER,
-		MY_SQL_PASSWORD: process.env.MY_SQL_PASSWORD,
-		DB_NAME: 'commissaire',
-		dialect: 'mysql',
-		pool: {
-			max: 5,
-			min: 0,
-			acquire: 30000,
-			idle: 10000,
-		},
-	},
+  database: {
+    MY_SQL_HOST: process.env.MY_SQL_HOST,
+    MY_SQL_USER: process.env.MY_SQL_USER,
+    MY_SQL_PASSWORD: process.env.MY_SQL_PASSWORD,
+    DB_NAME: "commissaire",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
 };
 
-
 const configByEnv = {
-	development: {
-		...baseConfig,
-		mode: 'development',
-	},
-	production: {
-		...baseConfig,
-		mode: 'production',
-	},
+  development: {
+    ...baseConfig,
+    mode: "development",
+  },
+  production: {
+    ...baseConfig,
+    mode: "production",
+  },
 };
 
 export default configByEnv[mode];
--- gb/server.js
+++ gb/server.js
@@ -1,75 +1,75 @@
-import cron from 'node-cron';
-import Sequelize from 'sequelize';
-import { configByEnv } from './config.js';
-const mode = process.env.MODE || 'development';
-const dbConfig = configByEnv[mode].database;
-import moment from 'moment';
+import cron from "node-cron";
+import Sequelize from "sequelize";
+import configByEnv from "./config.js";
+const mode = process.env.MODE || "development";
+const dbConfig = configByEnv.database;
+import moment from "moment";
 
 export const postStatus = {
-	OPEN: `open`,
-	CLOSED: `closed`,
-	DELETED: `deleted`,
-	NEW: `new`,
-	HOLD: `hold`,
+  OPEN: `open`,
+  CLOSED: `closed`,
+  DELETED: `deleted`,
+  NEW: `new`,
+  HOLD: `hold`,
 };
 
 export const serverConstants = {
-	MAX_MESSAGE_GENIE_WATCH_PER_DAY: 10,
-	MAX_MESSAGE_GENIE_ANSWER_PER_DAY: 10,
+  MAX_MESSAGE_GENIE_WATCH_PER_DAY: 10,
+  MAX_MESSAGE_GENIE_ANSWER_PER_DAY: 10,
 };
 
 const sequelize = new Sequelize(
-	dbConfig.DB_NAME,
-	dbConfig.USER||process.env.MY_SQL_USER,
-	dbConfig.PASSWORD|| process.env.MY_SQL_PASSWORD,
-	{
-		host: dbConfig.MY_SQL_HOST||process.env.MY_SQL_HOST,
-		port: dbConfig.PORT,
-		dialect: dbConfig.dialect,
-		dialectOptions: {
-			multipleStatements: true,
-		},
-		pool: {
-			max: dbConfig.pool.max,
-			min: dbConfig.pool.min,
-			acquire: dbConfig.pool.acquire,
-			idle: dbConfig.pool.idle,
-		},
-		logging: false,
-	},
+  dbConfig.DB_NAME,
+  dbConfig.USER || process.env.MY_SQL_USER,
+  dbConfig.PASSWORD || process.env.MY_SQL_PASSWORD,
+  {
+    host: dbConfig.MY_SQL_HOST || process.env.MY_SQL_HOST,
+    port: dbConfig.PORT,
+    dialect: dbConfig.dialect,
+    dialectOptions: {
+      multipleStatements: true,
+    },
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+    logging: false,
+  }
 );
 
 // Test the DB connection
 sequelize
-	.authenticate()
-	.then(() => {
-		console.log(
-			'Connection to the database has been established successfully.',
-		);
-	})
-	.catch((error) => {
-		console.error('Unable to connect to the database:', error);
-	});
+  .authenticate()
+  .then(() => {
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
 
 // SQL query function
 async function run() {
-	const transaction = await sequelize.transaction();
-	try {
-		console.log('Starting SQL execution...');
-		const SQL = `UPDATE genie_posts SET post_status='new',
+  const transaction = await sequelize.transaction();
+  try {
+    console.log("Starting SQL execution...");
+    const SQL = `UPDATE genie_posts SET post_status='new',
 						status_time=UTC_TIMESTAMP(),
 						genie_id=0  
 						WHERE post_status="hold" 
 						AND status_time <UTC_TIMESTAMP() - INTERVAL 10 MINUTE 
 						and is_block=0 and is_active=1`;
-		console.log('SQL:', SQL);
-		await sequelize.query(SQL, { transaction: transaction });
-		const yesterdayUTC = moment
-			.utc()
-			.subtract(1, 'day')
-			.format('YYYY-MM-DD HH:mm:ss');
+    console.log("SQL:", SQL);
+    await sequelize.query(SQL, { transaction: transaction });
+    const yesterdayUTC = moment
+      .utc()
+      .subtract(1, "day")
+      .format("YYYY-MM-DD HH:mm:ss");
 
-		const SQL2 = `
+    const SQL2 = `
 		UPDATE genie_users SET
     user_posts_count = CASE
       WHEN DATE(user_posts_count_date) < DATE('${yesterdayUTC}') THEN 0
@@ -102,16 +102,16 @@ async function run() {
     AND is_register = 1
     `;
 
-		console.log('SQL2:', SQL2);
-		await sequelize.query(SQL2, { transaction: transaction });
-		await transaction.commit();
+    console.log("SQL2:", SQL2);
+    await sequelize.query(SQL2, { transaction: transaction });
+    await transaction.commit();
 
-		const utcString = moment.utc().format('DD-MM-YYYY HH:mm:ss');
-		console.log('SQL execution completed.', utcString);
-	} catch (error) {
-		await transaction.rollback();
-		console.error('Error executing SQL:', error);
-	}
+    const utcString = moment.utc().format("DD-MM-YYYY HH:mm:ss");
+    console.log("SQL execution completed.", utcString);
+  } catch (error) {
+    await transaction.rollback();
+    console.error("Error executing SQL:", error);
+  }
 }
 
 // Stored procedure example
@@ -136,9 +136,9 @@ const cronExecuteFormat = `*/${executeMinutesParam} * * * *`; // EVERY X MINUTES
 
 // Cron job based on the custom format
 cron.schedule(cronExecuteFormat, async function () {
-	try {
-		run();
-	} catch (e) {
-		console.log(`e`, e);
-	}
+  try {
+    run();
+  } catch (e) {
+    console.log(`e`, e);
+  }
 });
